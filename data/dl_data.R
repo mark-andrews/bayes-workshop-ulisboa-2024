6a46b90f1ed7f22b85fb4e3ab6eff29dc46bd8c9
Mark Andrews <mjandrews.org@gmail.com>
2024-01-05 17:01:23 +0000

    Add data sets and slides

---
+++ data/dl_data.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+biochemists_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/biochemist.csv')
+
+smoking_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/smoking.csv')
+
+classroom_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/classroom.csv")
+
+mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieve.csv")
+mathach_school_df <- read_csv <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieveschool.csv")
+science_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/science.csv')
+owls <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/owls.csv')
+weight_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/main/data/weight.csv")
