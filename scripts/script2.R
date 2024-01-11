13bb7df42c4aa5a8bc836b4dd9e1408ddc29ef19
Mark Andrews <mark.andrews@ntu.ac.uk>
2024-01-11 18:06:28 +0000

    Update script2.R

--- scripts/script2.R
+++ scripts/script2.R
@@ -8,3 +8,16 @@ M_lm <- lm(y ~ x_1 + x_2, data = data_df1)
 
 # Bayes counterpart
 M_bayes_lm <- brm(y ~ x_1 + x_2, data = data_df1)
+
+plot(M_bayes_lm)
+
+mcmc_plot(M_bayes_lm, type = 'hist')
+
+mcmc_plot(M_bayes_lm, type = 'hist', variable = 'sigma')
+
+mcmc_plot(M_bayes_lm, type = "areas")
+mcmc_plot(M_bayes_lm, type = "areas_ridges")
+
+
+# what are my priors!
+prior_summary(M_bayes_lm)
