73e692182ca64773ec08bc9db9dc4e1ec7f63316
Mark Andrews <mark.andrews@ntu.ac.uk>
2024-01-12 15:48:48 +0000

    Update script3.R

--- scripts/script3.R
+++ scripts/script3.R
@@ -37,3 +37,47 @@ M3_b <- brm(weight ~ height + age + gender,
             cores = 4,
             prior = new_prior,
             data = weight_df)
+
+# Anova -------------------------------------------------------------------
+
+M4_f <- aov(score ~ gender * education_level, data = jobsatisfaction)
+summary(M4_f)
+
+
+M4_f_additive <- lm(score ~ gender + education_level, data = jobsatisfaction)
+M4_f_full <- lm(score ~ gender + education_level + gender:education_level, 
+                data = jobsatisfaction)
+
+anova(M4_f_additive, M4_f_full)
+summary(M4_f_additive)$r.sq
+summary(M4_f_full)$r.sq
+
+logLik(M4_f_additive)
+logLik(M4_f_full)
+
+AIC(M4_f_additive) - AIC(M4_f_full)
+
+
+# Bayesian way :)
+M4_b_additive <- brm(score ~ gender + education_level, 
+                     # save more info from sampler,
+                     # for use in loo and bayes_factor etc
+                     save_pars = save_pars(all = TRUE),
+                     data = jobsatisfaction)
+
+M4_b_full <- brm(score ~ gender + education_level + gender:education_level, 
+                 save_pars = save_pars(all = TRUE),
+                 data = jobsatisfaction)
+
+waic(M4_b_additive, M4_b_full)
+loo(M4_b_additive, M4_b_full)
+loo(M4_b_additive, M4_b_full, moment_match = TRUE)
+
+# log bayes factor model comparison 
+bayes_factor(M4_b_additive, M4_b_full, log = T)
+
+# interlude: nonnormal linear regression
+m_normal <- brm(dist ~ speed, data = cars)
+m_nonnormal <- brm(dist ~ speed, 
+                   family = student(), 
+                   data = cars)
