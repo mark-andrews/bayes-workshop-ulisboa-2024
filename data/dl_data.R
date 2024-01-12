37b3410e1549a9a6b77e5d0d9587cb80d2524533
Mark Andrews <mark.andrews@ntu.ac.uk>
2024-01-12 07:44:33 +0000

    Update the data dl script

--- data/dl_data.R
+++ data/dl_data.R
@@ -1,12 +1,14 @@
 library(tidyverse)
-biochemists_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/biochemist.csv')
+biochemists_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/biochemist.csv')
 
-smoking_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/smoking.csv')
+smoking_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/smoking.csv')
 
-classroom_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/classroom.csv")
+classroom_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/classroom.csv")
 
-mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieve.csv")
-mathach_school_df <- read_csv <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/mathachieveschool.csv")
-science_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/science.csv')
-owls <- read_csv('https://raw.githubusercontent.com/mark-andrews/ibdar23/master/data/owls.csv')
-weight_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/ibdar23/main/data/weight.csv")
+mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/mathachieve.csv")
+mathach_school_df <- read_csv <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/mathachieveschool.csv")
+science_df <- read_csv('https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/science.csv')
+owls <- read_csv('https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/master/data/owls.csv')
+weight_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/main/data/weight.csv")
+blp_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/main/data/blp-short2.csv")
+jobsatisfaction_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bayes-workshop-ulisboa-2024/main/data/jobsatisfaction.csv")
